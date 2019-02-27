4e5ff9cd728324bb272e723efb3ca4af1cb687ba
birensharma <birensharma@st.niituniversty.in>
2019-02-27 23:34:53 +0530

    init

--- app/src/main/java/com/example/evehicle/MainActivity.java
+++ app/src/main/java/com/example/evehicle/MainActivity.java
@@ -1,13 +1,76 @@
 package com.example.evehicle;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText email,password;
+    private Button log;
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser!=null)
+            navigate();
+        else
+            Toast.makeText(this, "Please login to continue", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email=findViewById(R.id.email);
+        password=findViewById(R.id.pass);
+        log=findViewById(R.id.log);
+        mAuth = FirebaseAuth.getInstance();
+
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail=email.getText().toString();
+                String pass=password.getText().toString();
+                if(!TextUtils.isEmpty(mail)&&!TextUtils.isEmpty(pass))
+                    login(mail,pass);
+                else
+                    Toast.makeText(MainActivity.this, "Fields are empty", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+    private void login(String email,String pass){
+        mAuth.signInWithEmailAndPassword(email,pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, ""+user.getEmail()
+                                    , Toast.LENGTH_SHORT).show();
+                            navigate();
+                        } else {
+                            Toast.makeText(getBaseContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    private void navigate(){
+        startActivity(new Intent(this,dashboard.class));
     }
 }
---
+++ app/src/main/java/com/example/evehicle/dashboard.java
@@ -0,0 +1,112 @@
+package com.example.evehicle;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class dashboard extends AppCompatActivity {
+
+    private static final int REQUEST_LOCATION = 123;
+    private FirebaseAuth auth;
+    private Button logout,getLoc;
+    private TextView txt, loc;
+    private double latitude = 0.0, longitude = 0.0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        requestPermission();
+        logout=findViewById(R.id.out);
+        txt=findViewById(R.id.text);
+        loc=findViewById(R.id.loc);
+        getLoc=findViewById(R.id.getloc);
+
+        String locationDetail="Latitide:"+latitude+"\nLongitude:"+longitude;
+
+        loc.setText(locationDetail);
+
+        auth=FirebaseAuth.getInstance();
+        FirebaseUser user=auth.getCurrentUser();
+        String u=user.getEmail();
+        txt.setText(u);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(dashboard.this, "Logging out...", Toast.LENGTH_SHORT).show();
+                auth.signOut();
+                finish();
+            }
+        });
+        getLoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLocation();
+            }
+        });
+
+    }
+    private void getLocation(){
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+           Toast.makeText(this, "Requesting permissions ", Toast.LENGTH_SHORT).show();
+            requestPermission();
+        }
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000, 10, new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            longitude = location.getLongitude();
+                            latitude = location.getLatitude();
+                            String locationDetail="Latitide:"+latitude+"\nLongitude:"+longitude;
+                            Toast.makeText(dashboard.this, locationDetail, Toast.LENGTH_SHORT).show();
+                            loc.setText(locationDetail);                        }
+
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {
+                            Toast.makeText(dashboard.this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        @Override
+                        public void onProviderEnabled(String provider) {
+                            Toast.makeText(dashboard.this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        @Override
+                        public void onProviderDisabled(String provider) {
+                            Toast.makeText(dashboard.this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+
+
+
+    }
+    public void  requestPermission(){
+
+        ActivityCompat.requestPermissions(this,
+
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION},
+                REQUEST_LOCATION);
+
+    }
+
+
+}
